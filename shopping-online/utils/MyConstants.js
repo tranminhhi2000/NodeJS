d07d6b0675b9227ba9f6cbe36f3f4c86c649fd53
tranminhhi2000 <tranminhhi2732000@gmail.com>
2020-06-21 17:55:44 +0700

    doing admin function

--- shopping-online/utils/MyConstants.js
+++ shopping-online/utils/MyConstants.js
@@ -1,10 +1,10 @@
 var MyConstants = {
-  DB_SERVER: 'aaa.mongodb.net',
-  DB_USER: 'bbb',
-  DB_PASS: 'ccc',
+  DB_SERVER: 'cluster0-8hpti.mongodb.net',
+  DB_USER: 'demoMongo',
+  DB_PASS: 'demo',
   DB_DATABASE: 'shoppingonline',
-  HOSTNAME: 'localhost:3000',
-  EMAIL_USER: 'eee@hotmail.com',
-  EMAIL_PASS: 'fff',
+  HOSTNAME: 'localhost:5000',
+  EMAIL_USER: 'hi.187pm23449@vanlanguni.vn',
+  EMAIL_PASS: 'VLU187pm23449',
 };
 module.exports = MyConstants;
