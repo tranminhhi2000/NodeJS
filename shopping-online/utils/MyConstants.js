efc6dd1ec91fd702d7047f8e4ecfbf122f7712cb
187PM20556 - Nguyen Thai Sang - K24T-PM1 <sang.187pm20556@vanlanguni.vn>
2020-06-22 23:00:52 +0700

    make it better

--- shopping-online/utils/MyConstants.js
+++ shopping-online/utils/MyConstants.js
@@ -3,7 +3,7 @@ var MyConstants = {
   DB_USER: 'demoMongo',
   DB_PASS: 'demo',
   DB_DATABASE: 'shoppingonline',
-  HOSTNAME: 'localhost:8000',
+  HOSTNAME: 'minhhi.herokuapp.com',
   EMAIL_USER: 'hi.187pm23449@vanlanguni.vn',
   EMAIL_PASS: 'VLU187pm23449',
 };
